041e12014f947f08f493f79fa3f47c029eccb87a
qttq23 <1753102@student.hcmus.edu.vn>
2021-04-01 17:02:55 +0700

    update output/

--- output/main.js
+++ output/main.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.combine = void 0;
 // import etc, {Account} from "./etc";
 var etc = require("./etc");
 console.log(new etc.Account('', 2));
@@ -7,3 +8,7 @@ var Car_1 = require("./model/Car");
 console.log(new Car_1.Car(88, 'carxs'));
 var randomstring = require("randomstring");
 console.log(randomstring.generate());
+function combine() {
+    return '' + new etc.Account('', 2) + new Car_1.Car(88, 'carxs') + randomstring.generate();
+}
+exports.combine = combine;
